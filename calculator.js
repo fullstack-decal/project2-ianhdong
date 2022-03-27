fa9c6c5c7ef3c38a58630615aed29361fc737a48
Ian Dong <90063441+ianhdong@users.noreply.github.com>
2022-03-26 21:50:21 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -7,7 +7,7 @@ let operator = null;
     Uncomment and fill in the blank spaces. */
 function calculations() {
     const intBuffer = parseInt(display); // Hint: Use parseInt to convert string to integer
-    if (operator === "+") {
+    if (operator === '+') {
         total += intBuffer;
     } else if (operator === '-') {
         total -= intBuffer;
@@ -20,17 +20,17 @@ function calculations() {
 
 /*   FUNC DESCRIPTION: If user input is a number, create the function. */
 function makesNumber(value) {
-    if (display === "0") {
-        display = input.toString();
+    if (display === '0') {
+        display = value.toString();
     } else {
     /*  If display is not 0, meaning there is a previous number typed in already, what should we display on the screen?
-    Hint: How do we concatenate strings? If you are stuck, imagine typing in a "5" into the calculator, making display into "5". 
-    Then imagine typing "3" into the calculator. Now "3" is value and display is still at "5", so display will now be 53.  */
-        display = display + input.toString();
+    Hint: How do we concatenate strings? If you are stuck, imagine typing in a '5' into the calculator, making display into '5'. 
+    Then imagine typing '3' into the calculator. Now '3' is value and display is still at '5', so display will now be 53.  */
+        display = display + value.toString();
     }
 }
 
-/*  FUNC DESCRIPTION: If user input is not a number, create the function. Create the functionality for "C", "←", "=", and operators. */
+/*  FUNC DESCRIPTION: If user input is not a number, create the function. Create the functionality for 'C', '←', '=', and operators. */
 
 function makesSymbol(symbol) {
     //make functionality for symbol C
@@ -50,7 +50,7 @@ function makesSymbol(symbol) {
             display = display.substring(0, display.length - 1);
         }
     } else if (symbol === '=') { //make functionality if symbol is an operator
-        calculate();
+        calculations();
         display = total.toString();
         total = 0;
         operator = null;
@@ -62,7 +62,7 @@ function makesSymbol(symbol) {
             calculations();
         }
         operator = symbol;
-        display = "0"; //stay consistent
+        display = '0'; //stay consistent
     }
 }
 
@@ -75,7 +75,7 @@ function setListeners() {
         console.log(item);
         item.addEventListener('click', function(event) {
             buttonClicked(event.target.innerText);
-        });
+        })
     //Hint: addEventListener might be useful.
     //Hint: event.target.innerText might be helpful. innerText return type is a string
     }
@@ -86,7 +86,7 @@ setListeners();
 
 /*  FUNC DESCRIPTION: Now we will write the function that takes care of when a button is clicked. */
 function buttonClicked(valueClicked) {
-    if (isNaN(parseInt(valueClicked))) { //NaN means "Not a Number"
+    if (isNaN(parseInt(valueClicked))) { //NaN means 'Not a Number'
         makesSymbol(valueClicked);
     } else {
         makesNumber(valueClicked);
